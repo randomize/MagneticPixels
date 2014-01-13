729165b6bab0d2edd45cb6b386da4528eca35c5d
randomize <randomize46@gmail.com>
2014-01-13 17:58:51 +0200

    Fixed indenting, damn default 4spaces, 3-space forever

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -11,46 +11,45 @@ AppDelegate::AppDelegate()
 
    // Create base loop
    master = new EMBaseMasterLoop();
-   //master->retain();
 
    // Early init (cocos not started yet)
    master->InitGame();
 
 }
 
-AppDelegate::~AppDelegate() 
+AppDelegate::~AppDelegate()
 {
 }
 
 bool AppDelegate::applicationDidFinishLaunching() {
-    // initialize director
-    auto director = Director::getInstance();
-    auto eglView = EGLView::getInstance();
 
-    director->setOpenGLView(eglView);
+   // initialize director
+   auto director = Director::getInstance();
+   auto eglView = EGLView::getInstance();
+
+   director->setOpenGLView(eglView);
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
-    eglView->setDesignResolutionSize(640, 960, ResolutionPolicy::SHOW_ALL );
+   eglView->setDesignResolutionSize(640, 960, ResolutionPolicy::SHOW_ALL);
 #else
-    eglView->setDesignResolutionSize(640, 960, ResolutionPolicy::NO_BORDER );
+   eglView->setDesignResolutionSize(640, 960, ResolutionPolicy::NO_BORDER );
 #endif
 
    auto frameSize = eglView->getFrameSize();
    auto size = director->getWinSize();
-	
 
 #ifdef MPIX_DEVELOPERS_BUILD
-    director->setDisplayStats(true);
+   director->setDisplayStats(true);
 #else
    director->setDisplayStats(false);
 #endif
-    
+
    // TODO: setup based on platform and resolution
    vector<string> v;
    v.push_back("Resources");
    FileUtils::getInstance()->setSearchPaths(v);
 
-    director->setAnimationInterval(1.0 / 60);
+   director->setAnimationInterval(1.0 / 60);
 
    CCLOG(" ");
    EM_LOG_INFO("==== Magnetic pixels started =====");
@@ -59,19 +58,19 @@ bool AppDelegate::applicationDidFinishLaunching() {
    // Always switch to start
    master->RunGame();
 
-    return true;
+   return true;
 }
 
 // This function will be called when the app is inactive. When comes a phone call,it's be invoked too
 void AppDelegate::applicationDidEnterBackground() {
    EM_LOG_INFO("MPix did enter BG");
-    Director::getInstance()->stopAnimation();
+   Director::getInstance()->stopAnimation();
    CmdDidEnterBG cmd; cmd.Execute();
 }
 
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground() {
    EM_LOG_INFO("MPix will enter FG");
-    Director::getInstance()->startAnimation();
+   Director::getInstance()->startAnimation();
    CmdWillEnterFG cmd; cmd.Execute();
 }
